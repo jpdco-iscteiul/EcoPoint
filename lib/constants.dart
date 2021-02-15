a80990c8a02f61bf613a5a1b3285836e77b4360a
jpdco <jpdco@iscte-iul.pt>
2021-02-15 16:22:21 +0000

    Logo Pictures Added

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF4C9A25);
+const kPrimaryLightColor= Color(0xFFA8E24C);
+const kConstrastColor = Color(0xFF2B2B2B);
