7d80ca7206832a2425c5130f3b903c87e959e7af
jpdco <jpdco@iscte-iul.pt>
2021-02-15 16:25:04 +0000

    Log in related Screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF4C9A25);
 const kPrimaryLightColor= Color(0xFFA8E24C);
-const kConstrastColor = Color(0xFF2B2B2B);
+const kContrastColor = Color(0xFF2B2B2B);
