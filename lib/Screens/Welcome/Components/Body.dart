900ef30a4d879f709480e3d838f25912c0a02249
jpdco <jpdco@iscte-iul.pt>
2021-02-18 12:53:21 +0000

    SignUp layout done

--- lib/Screens/Welcome/Components/Body.dart
+++ lib/Screens/Welcome/Components/Body.dart
@@ -1,22 +1,20 @@
 import 'package:eco_point_app/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size; // total height and wight of the screen
+    Size size =
+        MediaQuery.of(context).size; // total height and wight of the screen
     return Container(
-      padding: EdgeInsets.fromLTRB(25, 35, 25, 35),
+      padding: EdgeInsets.fromLTRB(25, 25, 25, 25),
       child: Column(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(
-                Icons.arrow_back_ios,
-                color: Colors.black,
-                  size: 30
-              ),
+              Icon(Icons.arrow_back_ios, color: Colors.black, size: 30),
               Text(
                 "Sign up",
                 style: TextStyle(
@@ -30,32 +28,85 @@ class Body extends StatelessWidget {
           Image.asset(
             'assets/images/Logo.png',
             //width: 600,
-           // height: 240,
+            //height: 240,
             fit: BoxFit.cover,
           ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Nome do utilizador'
-              ),
+          //form(context),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: TextFormField(
+                    decoration:
+                        InputDecoration(labelText: 'Nome do utilizador'),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: TextFormField(
+                    decoration:
+                        InputDecoration(labelText: 'Email do utilizador'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Esqueceu-se da palavra passe?"
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                    child: FlatButton(
+                      minWidth: size.width,
+                      color: kPrimaryColor,
+                      textColor: Colors.white,
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.black,
+                      padding: EdgeInsets.all(8.0),
+                      focusColor: kPrimaryLightColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      onPressed: () {
+                        /*...*/
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Já possui uma conta?', style: TextStyle(color: Colors.black, fontSize: 18),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: " Sign Up",
+                            style: TextStyle( color: kPrimaryLightColor)
+                          )
+                        ]
+                      ),
+                  ),
+                  ),
+                ),
+              ]),
             ),
           ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Email do utilizador'
-              ),
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-            ),
-          )
         ],
       ),
     );
