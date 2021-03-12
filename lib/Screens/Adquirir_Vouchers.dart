1d7e32f0b47eb47da371adda1aae9dcc46fe3e19
jpdco <jpdco@iscte-iul.pt>
2021-03-12 17:24:59 +0000

    Adquirir_Vouchers design begin.

--- lib/Screens/Adquirir_Vouchers.dart
+++ lib/Screens/Adquirir_Vouchers.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
+import '../constants.dart';
 import 'Welcome/Components/NavDrawer.dart';
 
 class Adquirir_Vouchers extends StatefulWidget{
@@ -14,11 +15,13 @@ class Adquirir_Vouchers extends StatefulWidget{
 class Adquirir_Vouchers_State extends State<Adquirir_Vouchers>{
   var marcas;
   var vouchers;
+  int pontos=0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getUserPoints();
     getVouchersAvailable();
   }
   @override
@@ -31,13 +34,58 @@ class Adquirir_Vouchers_State extends State<Adquirir_Vouchers>{
       appBar: AppBar(
         title: Text('Adquirir Vouchers'),
       ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(25, 50, 25, 25),
-      ),
+      body: Column(
+        children:[
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            height: 50,
+            width: 2*size.width/3,
+            decoration: BoxDecoration(
+              color: kPrimaryLightColor,
+              borderRadius: BorderRadius.all(Radius.circular(10))
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                pontos.toString()+" Pontos",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+            height: 50,
+            width: 2*size.width/3,
+            decoration: BoxDecoration(
+                color: kContrastColor,
+                borderRadius: BorderRadius.all(Radius.circular(10))
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                pontos.toString()+" Pontos",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(25, 50, 25, 25),
+          ),
+        ]
+      )
+
     );
   }
 
-  Future<void> getVouchersAvailable() async {
+  void getVouchersAvailable() async {
     Map<String, String> parameters = new HashMap<String,String>();
 
     var info;
@@ -48,10 +96,27 @@ class Adquirir_Vouchers_State extends State<Adquirir_Vouchers>{
     }
     );
     //print(vouchers);
-    vouchers = info[0];
-    marcas = info[1];
+    setState(() {
+      vouchers = info[0];
+      marcas = info[1];
+    });
+
     for(final x in marcas){
       print(x["objectId"]);
+    }
+  }
+  void getUserPoints() async {
+    var user = await ParseUser.currentUser();
+    print(user);
+
+    var queryBuilder = QueryBuilder(ParseObject("Detalhes_Conta"))
+      ..whereEqualTo("UserId", user);
+    var response = await queryBuilder.query();
+    if(response.success){
+      setState(pontos = response.result[0]["Pontos"]);
+    }
+    else{
+
     }
   }
 
